ccf30a8e613d3e055ef4fe38444496d863e823f3
zammsara <jsavalos@uamv.edu.ni>
2024-05-25 07:29:58 -0600

    trabajo de clase del 24/05/2024

---
+++ sesion24Mayo/sesion2405/funciones.cpp
@@ -0,0 +1,12 @@
+
+
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+void menu();
+void addReg ();
+void showReg();
+void editReg();
+void editReg();
+void findReg(string code);
---
+++ sesion24Mayo/sesion2405/variable.h
@@ -0,0 +1,16 @@
+
+#include <iostream>
+#include <string.h>
+using namespace std;
+#define MAX_REG 100
+
+typedef struct PRODUCT
+{
+    string code;
+    string name;
+    double price;
+    int stock;
+};
+
+PRODUCT products [MAX_REG];
+int pos = 0;
